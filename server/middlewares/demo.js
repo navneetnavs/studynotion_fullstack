910e3d70fa694915709488565756f1bd1dfaf8e8
navneetnavs <navneetkumar2004@icloud.com>
2025-07-11 10:52:36 +0530

    Commmit

--- server/middlewares/demo.js
+++ server/middlewares/demo.js
@@ -3,7 +3,7 @@
 //check if demo user
 exports.isDemo = async (req, res, next)=> {
     console.log(req.user.email);
-    if (req.user.email === "kumarhimanshusangwan@gmail.com" || req.user.email === "1234@gmail.com") {
+    if (req.user.email === "navneetkumar1800242@gmail.com" || req.user.email === "1234@gmail.com") {
         return res.status(401).json({
             success: false,
             message: "This is a Demo User",
